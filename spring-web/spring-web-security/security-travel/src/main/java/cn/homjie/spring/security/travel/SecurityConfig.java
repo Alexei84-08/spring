7a1b7a94d2138b0cf450d19f4eae923d8fc2e733
HomJie <1248823086@qq.com>
2017-11-07 11:30:29 +0800

    增加 security travel 配置和注解

--- spring-web/spring-web-security/security-travel/src/main/java/cn/homjie/spring/security/travel/SecurityConfig.java
+++ spring-web/spring-web-security/security-travel/src/main/java/cn/homjie/spring/security/travel/SecurityConfig.java
@@ -2,6 +2,8 @@ package cn.homjie.spring.security.travel;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,8 +15,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// @EnableWebSecurity 注解导入配置 WebSecurityConfiguration，将注册 springSecurityFilterChain 过滤器 Bean
 
 	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// 用于影响全局安全性的配置设置（忽略资源，设置调试模式，通过实现自定义防火墙定义拒绝请求）
+		web.ignoring().antMatchers("/resources/**");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// 控制权限，角色，URL 等安全
+		http
+			.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.permitAll()
+				.successForwardUrl("/logout.jsp");
+	}
+
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER")
-				.and().withUser("admin").password("password").roles("USER", "ADMIN");
+		// 建立认证机制
+		auth
+			.inMemoryAuthentication()
+				.withUser("user").password("password").roles("USER")
+				.and()
+				.withUser("admin").password("password").roles("USER", "ADMIN");
 	}
 }
