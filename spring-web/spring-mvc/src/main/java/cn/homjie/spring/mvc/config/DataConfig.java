de498a910b861b56e5c1d38e0b39dafbd076c3a4
HomJie <1248823086@qq.com>
2017-11-09 12:27:19 +0800

    开启 H2 Console 支持

--- spring-web/spring-mvc/src/main/java/cn/homjie/spring/mvc/config/DataConfig.java
+++ spring-web/spring-mvc/src/main/java/cn/homjie/spring/mvc/config/DataConfig.java
@@ -1,5 +1,6 @@
 package cn.homjie.spring.mvc.config;
 
+import org.h2.tools.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcOperations;
@@ -8,6 +9,7 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @Configuration
 public class DataConfig {
@@ -18,6 +20,22 @@ public class DataConfig {
 				.setType(EmbeddedDatabaseType.H2)
 				.addScript("schema.sql")
 				.build();
+	}
+
+	@Bean
+	public Server h2TcpServer() throws SQLException {
+		return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092").start();
+	}
+
+	/**
+	 * Web console for the embedded h2 database.
+	 * <p>
+	 * Go to http://localhost:8082 and connect to the database "jdbc:h2:mem:testdb", username "sa", password empty.
+	 * </p>
+	 */
+	@Bean
+	public Server h2WebServer() throws SQLException {
+		return Server.createWebServer("-web", "-webAllowOthers", "-webPort", "8082").start();
 	}
 
 	@Bean
